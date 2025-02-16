23f80ae5c5e1bdf76e30abdf80a635f7677eb808
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2025-02-16 21:28:15 +0530

    added

---
+++ Unit2_ClassesAndObjects/paraConstructor.java
@@ -0,0 +1,14 @@
+public class paraConstructor{
+
+
+    paraConstructor(){
+
+            System.out.println("Hello this is vishal");
+
+    }
+    public static void main(String[] args) {
+        
+      new paraConstructor();
+        
+    }
+}
